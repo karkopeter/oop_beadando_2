d705c34ba5b8b802ea71826e5668f8fdec378570
karko <karko@KARKO>
2023-05-02 18:53:33 +0200

    Add project files.

---
+++ oop_beadando_2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace oop_beadando_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            horgaszverseny horgaszverseny = new();
+            Caches caches = new Caches();
+
+            Console.WriteLine("Filenév:");
+
+            while (!horgaszverseny.IsFileOpen())
+            {
+                try
+                {
+                    string? ln = Console.ReadLine();
+
+                    if (ln == null)
+                    {
+                        continue;
+                    }
+
+                    horgaszverseny.OpenFile(ln);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hibás fájlnév!");
+                }
+            }
+
+            horgaszverseny.Start();
+
+
+            string MostCarp = "Ponty";
+            string Comp = "";
+            double FishWeight = 0;
+            string BiggestCarpCompetitor = "";
+            string MostFishCompetitor = "";
+
+            while (horgaszverseny.HasNext())
+            {
+                horgaszverseny.Next();
+                
+                // a
+                if (caches.FishWeight.Max() > FishWeight)
+                {
+                    FishWeight = caches.FishWeight.Max();
+                    Comp = horgaszverseny.Id;
+                    BiggestCarpCompetitor = horgaszverseny.Name;
+                }
+
+                //b
+
+                Dictionary<string, double> counts = new Dictionary<string, double>();
+
+                foreach(string fish in caches.FishName)
+                {
+                    if (counts.ContainsKey(fish))
+                    {
+                        counts[fish]++;
+                    }
+                    else
+                    {
+                        counts[fish] = 1;
+                    }
+                }
+                double maxCount = 0;
+                string maxFish = "";
+
+                foreach (var pair in counts)
+                {
+                    if (pair.Value > maxCount)
+                    {
+                        maxCount = pair.Value;
+                        maxFish = pair.Key;
+                    }
+                }
+
+                if(MostCarp == maxFish)
+                {
+                    MostFishCompetitor = horgaszverseny.Name;
+                }
+            }
+
+           
+            Console.WriteLine("Legnehezebb ponty súlya: " + FishWeight);
+            Console.WriteLine("Verseny: " + Comp + " versenyző: " + BiggestCarpCompetitor);
+            Console.WriteLine("Legtöbb ponty: " + MostCarp + " kifogta: " + MostFishCompetitor);
+            Console.WriteLine("Kész");
+        }
+    }
+}
---
+++ oop_beadando_2/horgaszverseny.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_beadando_2
+{
+    public class FileNoNextException : Exception { }
+    public class FileNotFoundException : Exception { }
+    public class FileLineNullException : Exception { }
+    public class WrongFileNameException : Exception { }
+
+    public class Caches
+    {
+        public string[]? FishName { get; set; }
+        public double[]? FishWeight { get; set; }
+    }
+
+    internal class horgaszverseny
+    {
+        public string Name { get; private set; }
+        public string Id { get; private set; }
+
+        public Caches FishData;
+
+        public horgaszverseny(Caches FishData)
+        {
+            this.FishData = FishData;
+            FishData.FishName = new string[1] { "Nincs kapás" };
+            FishData.FishWeight = new double[1] { 0 };
+        }
+
+        public void AddFish(string name, double weight)
+        {
+            FishData.FishName = FishData.FishName.Concat(new string[] { name }).ToArray();
+            FishData.FishWeight = FishData.FishWeight.Concat(new double[] { weight }).ToArray();
+        }
+
+        public void Start()
+        {
+
+            Name = "";
+            Id = "";
+            Caches Caches = new Caches { FishName = new string[1] { "Nincs kapás" }, FishWeight = new double[1] { 0 } };
+        }
+
+        public void Next()
+        {
+            if (!HasNext())
+            {
+                throw new FileNoNextException();
+            }
+
+            if (file == null) return;
+
+            string? line = file.ReadLine();
+
+            if (line == null)
+            {
+                throw new FileLineNullException();
+            }
+
+            string[] spls = line.Split(' ');
+
+            Name = spls[0];
+            Id = spls[1];
+
+            for (int i = 2; i < spls.Length; i += 2)
+            {
+                AddFish(spls[i], Convert.ToDouble(spls[i + 1]));
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (file == null)
+            {
+                return false;
+            }
+
+            return !file.EndOfStream;
+        }
+
+        public void OpenFile(string path)
+        {
+            if (file != null)
+            {
+                file.Close();
+            }
+
+            try
+            {
+                file = new StreamReader(path);
+            }
+            catch (Exception)
+            {
+                file = null;
+
+                throw new FileNotFoundException();
+            }
+        }
+
+        public bool IsFileOpen()
+        {
+            return file != null;
+        }
+
+        public horgaszverseny()
+        {
+            Name = "";
+            Id = "";
+            FishData = new Caches { FishName = new string[] { "" }, FishWeight = new double[] { 0 } };
+            file = null;
+        }
+
+        private StreamReader? file;
+    }
+}
+
